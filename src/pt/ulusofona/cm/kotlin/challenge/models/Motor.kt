5819bbbc1335defd70338585d30e32317e2c5615
Biel Souza <benevidessouza2014@gmail.com>
2022-02-21 12:44:06 +0000

    Ligavel

--- src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
@@ -1,12 +1,27 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 
-class Motor (val cavalos: Int, val cilindrada: Int){
+import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
+
+class Motor (val cavalos: Int, val cilindrada: Int): Ligavel{
 
     val ligado:Boolean = false
+    override fun ligar() {
+        TODO("Not yet implemented")
+    }
+
+    override fun desligar() {
+        TODO("Not yet implemented")
+    }
+
+    override fun estaLigado(): Boolean {
+        TODO("Not yet implemented")
+    }
 
     override fun toString(): String {
         return "Motor | $cavalos | $cilindrada"
     }
+
+
 
 
 }
